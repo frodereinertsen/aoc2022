555163fa0054bf1c0c7b09e7dcc5d3bc1e95a83f
Frode Reinertsen <frode.reinertsen@gmail.com>
2022-12-04 14:31:36 +0100

    day3

---
+++ src/main/kotlin/day3.kt
@@ -0,0 +1,17 @@
+fun sumOfItemsThatAreInBothHalfs(sacks: List<String>): Int = sacks.map { sack ->
+    sack.firstHalf() intersect sack.lastHalf()
+}.flatten().sumOf { it.priority }
+
+fun sumOfCommonsInAllGroups(sacks: List<String>): Int {
+    return sacks.map { it.toSet() }.windowed(3, 3) {
+        it.reduce { intersection, sack ->
+            intersection intersect sack
+        }
+    }.flatten().sumOf { it.priority }
+}
+
+private fun String.firstHalf(): Set<Char> = take(this.length / 2).toSet()
+private fun String.lastHalf(): Set<Char> = takeLast(this.length / 2).toSet()
+
+private val Char.priority
+    get() = code - if (isUpperCase()) 38 else 96
---
+++ src/test/kotlin/Day3Test.kt
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day3Test {
+
+    val exampleInput = """
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+""".trimIndent()
+
+    @Test
+    fun part1() {
+        assertEquals(157, sumOfItemsThatAreInBothHalfs(exampleInput.lines()))
+        assertEquals(7817, sumOfItemsThatAreInBothHalfs(readLines("day3.txt")))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(70, sumOfCommonsInAllGroups(exampleInput.lines()))
+        assertEquals(2444, sumOfCommonsInAllGroups(readLines("day3.txt")))
+    }
+}
