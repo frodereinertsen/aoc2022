f1b7a611f4dd4f5989947d13b1669eae90bcf69c
Frode Reinertsen <frode.reinertsen@gmail.com>
2022-12-04 16:44:43 +0100

    day4

---
+++ src/main/kotlin/day4.kt
@@ -0,0 +1,18 @@
+fun countPairWithOneFullyContainedInAnother(pairs: List<String>): Int = pairs.count { pair ->
+    val matches = getNumbers(pair)
+    val range1 = (matches[0] .. matches[1]).toSet()
+    val range2 = (matches[2] .. matches[3]).toSet()
+    range1.containsAll(range2) || range2.containsAll(range1)
+}
+
+fun countPairsThatOverlap(pairs: List<String>): Int = pairs.count { pair ->
+    val matches = getNumbers(pair)
+    val range1 = (matches[0] .. matches[1]).toSet()
+    val range2 = (matches[2] .. matches[3]).toSet()
+    (range1 intersect range2).isNotEmpty()
+}
+
+val numbersRegex = Regex("(\\d+)-(\\d+),(\\d+)-(\\d+)")
+
+fun getNumbers(pair: String): List<Int> =
+    numbersRegex.find(pair)?.groups?.toList()?.subList(1, 5)?.map { it?.value?.toInt()!! } ?: throw RuntimeException("yukk")
---
+++ src/test/kotlin/Day4Test.kt
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Day4Test {
+    val exampleInput = """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+""".trimIndent()
+
+    @Test
+    fun part1() {
+        assertEquals(2, countPairWithOneFullyContainedInAnother(exampleInput.lines()))
+        assertEquals(569, countPairWithOneFullyContainedInAnother(readLines("day4.txt")))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(4, countPairsThatOverlap(exampleInput.lines()))
+        assertEquals(936, countPairsThatOverlap(readLines("day4.txt")))
+    }
+}
