414acf7c2a8f040308ef2253a4060012e8e929ad
Frode Reinertsen <frode.reinertsen@gmail.com>
2022-12-13 09:51:18 +0100

    asdf

--- src/main/kotlin/day12/day12.kt
+++ src/main/kotlin/day12/day12.kt
@@ -1,85 +1,33 @@
 package day12
 
-fun main() {
-    val nodes = 6
-    val edges: Array<IntArray> = Array(nodes) { IntArray(nodes) { -1 } }
-    edges[0][1] = 34
-    edges[0][3] = 43
-    edges[1][2] = 56
-    edges[1][3] = 20
-    edges[1][4] = 57
-    edges[2][5] = 20
-    edges[3][1] = 20
-    edges[3][4] = 50
-    edges[4][2] = 37
+fun part1(input: List<String>): Int {
+    val parsed = parseInput(input)
+    val map = HeightMap(parsed)
+    val graph = map.createGraphPart1()
+    val shortestPathTree: Map<Coordinate, Coordinate?> = dijkstra(graph, map.start.first)
 
-    dijkstra(0, edges, nodes)
+    val shortestPath = shortestPath(shortestPathTree, map.start.first, map.end.first)
+
+    return shortestPath.count() - 1
 }
 
-fun dijkstra(source: Int, edges: Array<IntArray>, nodes: Int) {
-    // Initialize single source
-    val d = IntArray(nodes) { Integer.MAX_VALUE }
-    val pi = IntArray(nodes) { -1 }
-    d[source] = 0
+fun part2(input: List<String>): Int {
+    val parsed = parseInput(input)
+    val map = HeightMap(parsed)
+    val graph = map.createGraphPart1()
+    val shortestPathTree: Map<Coordinate, Coordinate?> = dijkstra(graph, map.start.first)
 
-    val S: MutableList<Int> = ArrayList()
-    val Q: MutableList<Int> = (0 until nodes).toMutableList()
+    val shortestPath = shortestPath(shortestPathTree, map.start.first, map.end.first)
 
-    // Iterations
-    while (Q.isNotEmpty()) {
-        val u: Int = extractMin(Q, d)
-        S.add(u)
-
-        edges[u].forEachIndexed { v, vd ->
-            if (vd != -1 && d[v] > d[u] + vd) {
-                d[v] = d[u] + vd
-                pi[v] = u
-            }
-        }
-    }
-
-    println("d: ${d.contentToString()}")
-    println("pi: ${pi.contentToString()}")
+    return shortestPath.count() - 1
 }
 
-fun extractMin(Q: MutableList<Int>, d: IntArray): Int {
-    var minNode = Q[0]
-    var minDistance: Int = d[0]
+typealias Coordinate = Pair<Int, Int>
+typealias Coordinates = Map<Coordinate, Char>
 
-    Q.forEach {
-        if (d[it] < minDistance) {
-            minNode = it
-            minDistance = d[it]
-        }
-    }
-
-    Q.remove(minNode)
-    return minNode
-}
-
-fun <T> List<Pair<T, T>>.getUniqueValuesFromPairs(): Set<T> = map { (a, b) -> listOf(a, b) }
-    .flatten()
-    .toSet()
-
-fun <T> List<Pair<T, T>>.getUniqueValuesFromPairs(predicate: (T) -> Boolean): Set<T> = map { (a, b) -> listOf(a, b) }
-    .flatten()
-    .filter(predicate)
-    .toSet()
-
-data class Graph<T>(
-    val vertices: Set<T>,
-    val edges: Map<T, Set<T>>,
-    val weights: Map<Pair<T, T>, Int>
-) {
-    constructor(weights: Map<Pair<T, T>, Int>) : this(
-        vertices = weights.keys.toList().getUniqueValuesFromPairs(),
-        edges = weights.keys
-            .groupBy { it.first }
-            .mapValues { it.value.getUniqueValuesFromPairs { x -> x !== it.key } }
-            .withDefault { emptySet() },
-        weights = weights
-    )
-}
+fun parseInput(input: List<String>): Coordinates = input.map { it.toCharArray() }.toTypedArray()
+    .flatMapIndexed { rowIndex, line -> line.mapIndexed { columnIndex, char -> (rowIndex to columnIndex) to char } }
+    .toMap()
 
 fun <T> dijkstra(graph: Graph<T>, start: T): Map<T, T?> {
     val S: MutableSet<T> = mutableSetOf() // a subset of vertices, for which we know the true distance
@@ -87,7 +35,7 @@ fun <T> dijkstra(graph: Graph<T>, start: T): Map<T, T?> {
     val delta = graph.vertices.map { it to Int.MAX_VALUE }.toMap().toMutableMap()
     delta[start] = 0
 
-    val previous: MutableMap<T, T?> = graph.vertices.map { it to null }.toMap().toMutableMap()
+    val previous: MutableMap<T, T?> = graph.vertices.associateWith { null }.toMutableMap()
 
     while (S != graph.vertices) {
         val v: T = delta
@@ -119,58 +67,91 @@ fun <T> shortestPath(shortestPathTree: Map<T, T?>, start: T, end: T): List<T> {
     return pathTo(start, end)
 }
 
-val input = """
-    Sabqponm
-    abcryxxl
-    accszExk
-    acctuvwj
-    abdefghi
-""".trimIndent()
+fun <T> List<Pair<T, T>>.getUniqueValuesFromPairs(): Set<T> = map { (a, b) -> listOf(a, b) }
+    .flatten()
+    .toSet()
 
-typealias Coordinate = Pair<Int, Int>
-typealias Coordinates = Map<Coordinate, Char>
-typealias CoordinateValue = Pair<Coordinate, Char>
+fun <T> List<Pair<T, T>>.getUniqueValuesFromPairs(predicate: (T) -> Boolean): Set<T> = map { (a, b) -> listOf(a, b) }
+    .flatten()
+    .filter(predicate)
+    .toSet()
 
+data class Graph<T>(
+    val vertices: Set<T>,
+    val edges: Map<T, Set<T>>,
+    val weights: Map<Pair<T, T>, Int>
+) {
+    constructor(weights: Map<Pair<T, T>, Int>) : this(
+        vertices = weights.keys.toList().getUniqueValuesFromPairs(),
+        edges = weights.keys
+            .groupBy { it.first }
+            .mapValues { it.value.getUniqueValuesFromPairs { x -> x !== it.key } }
+            .withDefault { emptySet() },
+        weights = weights
+    )
+}
 
-fun parseInput(input: List<String>): Coordinates = input.map { it.toCharArray() }.toTypedArray()
-    .flatMapIndexed { rowIndex, line -> line.mapIndexed { columnIndex, char -> (rowIndex to columnIndex) to char } }
-    .toMap()
-
-class HeightMap(private val coordinates: Coordinates) {
+class HeightMap(val coordinates: Coordinates) {
     private val maxRowIndex = coordinates.keys.maxBy { it.first }.first
     private val maxColumnIndex = coordinates.keys.maxBy { it.second }.second
 
-    fun createGraph() {
-        val weights = mutableSetOf<Pair<Pair<Char, Char>, Int>>()
+    fun createGraphPart1(): Graph<Pair<Int, Int>> {
+        val weights = mutableMapOf<Pair<Coordinate, Coordinate>, Int>()
 
-        for (rowIndex in 0 ..maxRowIndex) {
-            for (columnIndex in 0 ..maxColumnIndex) {
+        for (rowIndex in 0..maxRowIndex) {
+            for (columnIndex in 0..maxColumnIndex) {
                 val coordinate = rowIndex to columnIndex
                 val elevation = getSymbol(coordinate).elevation()
-                coordinate.left()?.let {
-                    val otherElevation = getSymbol(it).elevation()
-                    if (elevation in otherElevation .. otherElevation + 1 ) {
-                        weights.add(getSymbol(coordinate) to getSymbol(it) to 1)
+                listOfNotNull(
+                    coordinate.left(),
+                    coordinate.right(),
+                    coordinate.over(),
+                    coordinate.under()
+                ).forEach { other ->
+                    val otherElevation = getSymbol(other).elevation()
+                    if (elevation + 1 >= otherElevation) {
+                        weights[coordinate to other] = 1
                     }
-                }
-                coordinate.right()?.let {
-
-                }
-                coordinate.over()?.let {
-
-                }
-                coordinate.under()?.let {
-
                 }
             }
         }
+
+        return Graph(weights)
+    }
+
+    fun createGraphPart2(): Graph<Pair<Int, Int>> {
+        val weights = mutableMapOf<Pair<Coordinate, Coordinate>, Int>()
+
+        for (rowIndex in 0..maxRowIndex) {
+            for (columnIndex in 0..maxColumnIndex) {
+                val coordinate = rowIndex to columnIndex
+                val elevation = getSymbol(coordinate).elevation()
+                listOfNotNull(
+                    coordinate.left(),
+                    coordinate.right(),
+                    coordinate.over(),
+                    coordinate.under()
+                ).forEach { other ->
+                    val otherElevation = getSymbol(other).elevation()
+                    if (elevation + 1 >= otherElevation) {
+                        weights[coordinate to other] = 1
+                    }
+                }
+            }
+        }
+
+        return Graph(weights)
     }
 
     private fun getSymbol(coordinate: Pair<Int, Int>) =
         coordinates[coordinate] ?: throw RuntimeException("could not find coordinate")
 
-    private val start by lazy {
+    val start by lazy {
         coordinates.firstNotNullOf { if (it.value == 'S') it else null }.toPair()
+    }
+
+    val end by lazy {
+        coordinates.firstNotNullOf { if (it.value == 'E') it else null }.toPair()
     }
 
     private fun Coordinate.left(): Coordinate? {
@@ -183,7 +164,7 @@ class HeightMap(private val coordinates: Coordinates) {
     private fun Coordinate.right(): Coordinate? {
         val (rowIndex, columnIndex) = this
         return if (rowIndex + 1 <= maxRowIndex) {
-            rowIndex - 1 to columnIndex
+            rowIndex + 1 to columnIndex
         } else null
     }
 
@@ -201,9 +182,9 @@ class HeightMap(private val coordinates: Coordinates) {
         } else null
     }
 
-    private fun Char.elevation(): Int = when(this) {
+    private fun Char.elevation(): Int = when (this) {
         'S' -> 'a'
         'E' -> 'z'
         else -> this
-    }.digitToInt()
+    }.code
 }
--- src/test/kotlin/day12/Day12Test.kt
+++ src/test/kotlin/day12/Day12Test.kt
@@ -2,6 +2,7 @@ package day12
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import readLines
 
 internal class Day12Test {
     @Test
@@ -21,5 +22,25 @@ internal class Day12Test {
         assertEquals(listOf(start, "B", "C"), shortestPath(shortestPathTree, start, "C"))
         assertEquals(listOf(start, "B", "C", "E"), shortestPath(shortestPathTree, start, "E"))
         assertEquals(listOf(start, "D"), shortestPath(shortestPathTree, start, "D"))
+    }
+
+    val input = """
+        Sabqponm
+        abcryxxl
+        accszExk
+        acctuvwj
+        abdefghi
+    """.trimIndent()
+
+    @Test
+    fun part1() {
+        assertEquals(31, part1(input.lines()))
+        assertEquals(481, part1(readLines("day12.txt")))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(31, part2(input.lines()))
+        assertEquals(31, part2(readLines("day12.txt")))
     }
 }
